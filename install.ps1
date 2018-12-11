041b0e59d6a087f013ed11499f7272919c108065
joniluomala <joni.luomala@gmail.com>
2018-12-11 10:50:31 +0200

    functions

--- install.ps1
+++ install.ps1
@@ -1,2 +1,3 @@
 
-New-AzureRmResourceGroupDeployment -ResourceGroupName IoTEnvironment -TemplateFile https://raw.githubusercontent.com/joniluomala/iot-env/master/TimeSeriesInsights.json -TemplateParameterFile $parametersFilePath;
+New-AzureRmResourceGroupDeployment -ResourceGroupName IoTEnvironment -TemplateFile https://raw.githubusercontent.com/joniluomala/iot-env/master/functions/template.json -TemplateParameterFile $parametersFilePath;
+New-AzureRmResourceGroupDeployment -ResourceGroupName IoTEnvironment -TemplateFile https://raw.githubusercontent.com/joniluomala/iot-env/master/storage/template.json -TemplateParameterFile  https://raw.githubusercontent.com/joniluomala/iot-env/master/storage/parameters.json
